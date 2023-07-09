3ce615f2a5c341283651b78c23cc0a6d3a6e0d24
malikayan827 <malikayan827@gmail.com>
2023-07-09 13:56:13 +0500

    Added check UI functionality

--- script.js
+++ script.js
@@ -1,52 +1,71 @@
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
-const clearBtn=document.getElementById('clear');
-function addItem(e){
-    const newItemText = itemInput.value;
-    e.preventDefault();
-    //validate
-    if(newItemText === ''){
-  alert('please enter a value');
-  return;
-    }
-    //list item
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
-   const li=document.createElement('li');
-   li.appendChild(document.createTextNode(newItemText));
-   const button= createButton('remove-item btn-link text-red');
-   li.appendChild(button);
-    itemList.appendChild(li);
-    itemInput.value='';
+function addItem(e) {
+  const newItemText = itemInput.value;
+  e.preventDefault();
+  //validate
+  if (newItemText === "") {
+    alert("please enter a value");
+    return;
+  }
+  //list item
 
-};
-function createButton(cssClass){
-    const button = document.createElement('button');
-    button.className=cssClass;
-    const icon = createIcon('fa-solid fa-xmark');
-    button.appendChild(icon);
-    return button;
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItemText));
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  // add lis to Dom
+
+  itemList.appendChild(li);
+  checkUI();
+  itemInput.value = "";
 }
-function createIcon(cssClass){
-    const icon = document.createElement('i');
-    icon.className=cssClass;
+function createButton(cssClass) {
+  const button = document.createElement("button");
+  button.className = cssClass;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+function createIcon(cssClass) {
+  const icon = document.createElement("i");
+  icon.className = cssClass;
+
+  return icon;
+}
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if(window.confirm("Are you sure?")){
+    e.target.parentElement.parentElement.remove();}
+    checkUI();
+}
+}
+function clearItem(e) {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
     
-    return icon;
+  }
+  checkUI();
 }
-function removeItem(e){
-    if(e.target.parentElement.classList.contains('remove-item')) {
-        e.target.parentElement.parentElement.remove();
-    }
-    
-}
-function clearItem(e){
-  while(itemList.firstChild){
-      itemList.removeChild(itemList.firstChild);
+function checkUI() {
+  const Items = itemList.querySelectorAll("li");
+  console.log(Items);
+  if (Items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
   }
 }
 
 //events
-itemForm.addEventListener('submit',addItem);
-itemList.addEventListener('click',removeItem);
-clearBtn.addEventListener('click',clearItem);
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItem);
 
+checkUI();
